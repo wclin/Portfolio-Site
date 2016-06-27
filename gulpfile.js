f2ce1eef8adc7f1dc87a877ab62d98d9374d9b66
wclin <r03922092+git@csie.ntu.edu.tw>
2016-06-27 23:48:44 +0800

    Minify css workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css'),
+    browserSync = require('browser-sync').create();
 
-// Static Server + watching html files
+// Static Server
 gulp.task('browse', function() {
 
     browserSync.init({
         server: "./framework"
     });
+});
 
-    gulp.watch("framework/css/*.css").on('change', browserSync.reload);
+// Minify css
+gulp.task('styles', function(){
+    gulp.src('framework/css/*.css')
+        .pipe(minifyCSS())
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+        }))
+        .pipe(gulp.dest('framework/css/'));
+});
+
+// Watching html and css files
+gulp.task('watch', function(){
+    gulp.watch('css/**/*.css', ['styles']);
+    gulp.watch("framework/css/*.min.css").on('change', browserSync.reload);
     gulp.watch("framework/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['browse']);
+
+gulp.task('default', ['browse', 'styles', 'watch']);
